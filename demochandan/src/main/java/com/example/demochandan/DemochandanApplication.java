f4c1d0536845b7fbe957fa64b2fb0d3f1994d18b
chandan505 <chandanvaday360@gmail.com>
2020-01-25 03:42:03 -0800

    spring boot initial commit

---
+++ demochandan/src/main/java/com/example/demochandan/DemochandanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demochandan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemochandanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemochandanApplication.class, args);
+	}
+
+}
